ac7fe997d4d589cd3b67d1b59528b7f44d06bf20
lucafan8973 <ferexmoto6@gmail.com>
2025-12-23 20:30:37 +0900

    first commit

---
+++ apps/dapp/src/app.tsx
@@ -0,0 +1,30 @@
+import { Provider } from '@defi-token/blockchain';
+import Home from './pages/home';
+import { QueryClient } from '@tanstack/react-query';
+import { Route, Routes } from 'react-router-dom';
+import NotFoundPage from './pages/not-found';
+import SignInPage from './pages/sign-in';
+
+declare global {
+  interface Window {
+    WidgetApp_v1?: {
+      mount: (elementId: string, option: { userId: string}) => void;
+      unmount?: () => void;
+    }
+  }
+}
+
+export function App() {
+  const queryClient = new QueryClient();
+  return (
+    <Provider configProvider="rainbow" queryClient={queryClient}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Provider>
+  );
+}
+
+export default App;
