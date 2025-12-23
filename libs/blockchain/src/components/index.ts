6f21ad155a0a271979b138a9219786ad566796b2
lucafan8973 <ferexmoto6@gmail.com>
2025-12-23 22:01:08 +0900

    one

--- libs/blockchain/src/components/index.ts
+++ libs/blockchain/src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './Provider';
 export * from './connect-button';
-export * from './metamask/metamask-provider';
 export * from './rainbow/rainbow-connect-button';
 export * from './wagmi/wagmi-connect-button';
 export * from './wagmi/wallet-account';
